e5681cbd336ef78b0d3ec5a6af2d33edc58998ac
Swastik Ghosh <106193184+sg1106@users.noreply.github.com>
2025-06-03 21:18:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -68,4 +68,5 @@ def general_query():
     return render_template('index.html', result=answer)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
